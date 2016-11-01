13a06e25d4c53a324b54fc80b9faf9a24474988a
dongdong108 <liudongdong108@163.com>
2016-11-01 15:49:58 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/viewtext/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.bw.viewtext;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btn = (Button) findViewById(R.id.btnn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
